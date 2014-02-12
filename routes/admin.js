8923a2005b1f793b6c80bccc088c6d4de8ecd434
spantons <spantons@gmail.com>
2014-02-11 20:35:17 -0430

    Subir multiples archivos, bug: too many connections

--- routes/admin.js
+++ routes/admin.js
@@ -1,260 +1,176 @@
 var fs = require('fs')
+	, readline = require('readline')
 	, path = require('path')
 	, lineReader = require('line-reader')
 	, sanitize = require('validator').sanitize
-	, BD = require('../BD');
+	, BD = require('../BD')
+	, async = require('async');
 
 exports.syncUpload = function(req, res){
+	
+	if(req.body.zona_admin != undefined || req.body.new_zone != undefined){
 
-	if(req.body.zona_admin != '' || req.body.new_zone != '' || req.body.id_place != ''){
+		zonaAdmin = sanitize(req.body.zona_admin).xss();
+		zonaAdmin = sanitize(zonaAdmin).entityDecode();
+	
+		newZone = sanitize(req.body.new_zone).xss();
+		newZone = sanitize(newZone).entityDecode();
 		
-		zona_admin = sanitize(req.body.zona_admin).xss();
-		zona_admin = sanitize(zona_admin).entityDecode();
+		if(req.files.data_measures.length > 0){
+			findPlaceId(zonaAdmin,newZone,req.files.data_measures,res);
+			res.send('/admin');
 		
-		new_zone = sanitize(req.body.new_zone).xss();
-		new_zone = sanitize(new_zone).entityDecode();
-		
-		id_place = sanitize(req.body.id_place).xss();
-		id_place = sanitize(id_place).entityDecode();
-		
-		//int id_place;
-		
-		var tempPathDataMeasures = req.files.data_measures;
-		if(tempPathDataMeasures != null){
-		
-			if (path.extname(tempPathDataMeasures.name).toLowerCase() === '.txt') {
-			
-				var targetPathDataMeasures = "public/uploads/measures/" + tempPathDataMeasures.name;
-				fs.rename(tempPathDataMeasures.path, targetPathDataMeasures, function(err) {
-		    	if (err)	
-		        res.send('3'); 
-		      else {
-			    	fs.unlink(tempPathDataMeasures.path, function() {
-				    	if (err)
-				      	res.send('3'); 
-							else {
-							
-			          // PASO TODO --------------------------------------------------------------------------------
-			          fs.chmodSync(targetPathDataMeasures, 0777);
-			          
-			          // Si existe una nueva zona la agregamos --------------------------------------------------------------------------------
-			          if(new_zone != ""){
-					  
-									objBD = BD.BD();
-									objBD.connect();
-									objBD.query("INSERT INTO places (name) VALUES ("+ objBD.escape(new_zone) +")",
-									function(err, rows, fields) {
-								    if (err){
-								    	console.log(err);
-											res.send('3');
-										}
-									});
-									
-									zona_admin = new_zone;
-									
-									
-									objBD.query("SELECT id_place FROM places WHERE name = "+ objBD.escape(zona_admin) +"",
-									function(err, rows, fields) {
-								    if (err){
-										//console.log("Out");
-								    	console.log(err);
-											res.send('3'); 
-										//	console.log("In");
-										} 
-										
-										else {
-										console.log("In");
-											id_place = rows[0].id_place;	
-												console.log("Id_Place: " + id_place);
-											// Arrays dinamicos que me guardan frecuencia_potencia y coordenadas --------------------------------------------------------------------------------									
-											var frequency_potency = new Array();												
-											var coordinate = new Array();
-											// Leemos las lineas del archivo --------------------------------------------------------------------------------
-											lineReader.eachLine(targetPathDataMeasures, function(line, last) {
-											  line_split = line.split("\t");	
-												
-												if(line_split.length == 2)
-													frequency_potency.push(line_split);
-													
-												else if(line_split.length == 1)
-													coordinate.push(line_split);
-											
-											  if (last) {
-													// Guardamos coordenadas en la DB --------------------------------------------------------------------------------
-													objBD = BD.BD();
-													objBD.connect();
-													objBD.query("INSERT INTO coordinates (latitude, longitude, id_place, date) VALUES ("+ objBD.escape(coordinate[0]) +","+ objBD.escape(coordinate[1]) +","+ objBD.escape(id_place) +","+ objBD.escape(coordinate[2]) +")",
-														function(err, rows, fields) {
-													    if (err){
-													    	console.log(err);
-																res.send('3'); 
-															} else {
-																// Guardamos potencia y frecuencia en la DB --------------------------------------------------------------------------------
-																id_coordinates = rows.insertId;
-																
-																for(i = 0; i < frequency_potency.length; i++){
-																	objBD = BD.BD();
-																	objBD.connect();
-																	objBD.query("INSERT INTO potency_frequency (frequency, potency) VALUES ("+ objBD.escape(frequency_potency[i][0]) +","+ objBD.escape(frequency_potency[i][1]) +")",
-																		function(err, rows, fields) {
-																	    if (err){
-																	    	console.log(err);
-																				res.send('3'); 
-																			} else {
-																				// Guardamos id de potencia y frecuencia y de coordenadas en la tabla compartida de la DB ---------------------------------------------------------------
-																				id_potency_frequency = rows.insertId;
-																				
-																				objBD = BD.BD();
-																				objBD.connect();
-																				objBD.query("INSERT INTO coordinates_vs_potency_frequency (id_potency_frequency, id_coordinate) VALUES ("+ id_potency_frequency +","+ id_coordinates +")",
-																					function(err, rows, fields) {
-																				    if (err){
-																				    	console.log(err);
-																							res.send('3'); 
-																						} else {
-																							// Guardado todo, recargamos la pagina
-																							res.send('/admin');
-																						}
-																				});
-																				objBD.end();
-																			}
-																	});
-																	objBD.end();
-																}
-															}
-													});
-													objBD.end();
-													// Salimos de la lectura del archivos --------------------------------------------------------------------------------
-											    return false; 
-											  }
-											});												
-													
-										}
-								});         
-								//objBD.end();
-									
-									
-									
-									}
-									objBD.end();
-									//zona_admin = new_zone;
-									
-									
-									if(zona_admin == new_zone){
-									
-									// Buscamos el id de la zona --------------------------------------------------------------------------------
-			          
-								
-									/*console.log("El lugar es ");
-									console.log(zona_admin);*/
-									
- 
-									
-			          }
-			          
-					  
-					  
-					  
-					  
-			          // Buscamos el id de la zona --------------------------------------------------------------------------------
-			          /*objBD = BD.BD();
-								objBD.connect();
-								console.log("In ");
-								console.log(zona_admin);
-								objBD.query("SELECT id_place FROM places WHERE name = "+ objBD.escape(zona_admin) +"",
-									function(err, rows, fields) {
-								    if (err){
-										//console.log("Out");
-								    	console.log(err);
-											res.send('3'); 
-										//	console.log("In");
-										} 
-										
-										else {
-										console.log("In");
-										//if(!err){
-											id_place = rows[0].id_place;	
-												console.log("Id_Place: " + id_place);
-											// Arrays dinamicos que me guardan frecuencia_potencia y coordenadas --------------------------------------------------------------------------------									
-											var frequency_potency = new Array();												
-											var coordinate = new Array();
-											// Leemos las lineas del archivo --------------------------------------------------------------------------------
-											lineReader.eachLine(targetPathDataMeasures, function(line, last) {
-											  line_split = line.split("\t");	
-												
-												if(line_split.length == 2)
-													frequency_potency.push(line_split);
-													
-												else if(line_split.length == 1)
-													coordinate.push(line_split);
-											
-											  if (last) {
-													// Guardamos coordenadas en la DB --------------------------------------------------------------------------------
-													objBD = BD.BD();
-													objBD.connect();
-													objBD.query("INSERT INTO coordinates (latitude, longitude, id_place, date) VALUES ("+ objBD.escape(coordinate[0]) +","+ objBD.escape(coordinate[1]) +","+ objBD.escape(id_place) +","+ objBD.escape(coordinate[2]) +")",
-														function(err, rows, fields) {
-													    if (err){
-													    	console.log(err);
-																res.send('3'); 
-															} else {
-																// Guardamos potencia y frecuencia en la DB --------------------------------------------------------------------------------
-																id_coordinates = rows.insertId;
-																
-																for(i = 0; i < frequency_potency.length; i++){
-																	objBD = BD.BD();
-																	objBD.connect();
-																	objBD.query("INSERT INTO potency_frequency (frequency, potency) VALUES ("+ objBD.escape(frequency_potency[i][0]) +","+ objBD.escape(frequency_potency[i][1]) +")",
-																		function(err, rows, fields) {
-																	    if (err){
-																	    	console.log(err);
-																				res.send('3'); 
-																			} else {
-																				// Guardamos id de potencia y frecuencia y de coordenadas en la tabla compartida de la DB ---------------------------------------------------------------
-																				id_potency_frequency = rows.insertId;
-																				
-																				objBD = BD.BD();
-																				objBD.connect();
-																				objBD.query("INSERT INTO coordinates_vs_potency_frequency (id_potency_frequency, id_coordinate) VALUES ("+ id_potency_frequency +","+ id_coordinates +")",
-																					function(err, rows, fields) {
-																				    if (err){
-																				    	console.log(err);
-																							res.send('3'); 
-																						} else {
-																							// Guardado todo, recargamos la pagina
-																							res.send('/admin');
-																						}
-																				});
-																				objBD.end();
-																			}
-																	});
-																	objBD.end();
-																}
-															}
-													});
-													objBD.end();
-													// Salimos de la lectura del archivos --------------------------------------------------------------------------------
-											    return false; 
-											  }
-											});												
-													
-										}
-								});         
-								objBD.end();*/
-			        }
-			      });
-			    }
-		    });
-			
+		} else 
+		  res.send('1');
+		  
+	} else 
+	  res.send('0');
+};
+
+
+
+// Guarda y/o busca id lugar -------------------------------------------------------------------
+findPlaceId = function(zonaAdmin,newZone,files,res){
+
+	if(zonaAdmin == '' && newZone != ''){
+		objBD = BD.BD();
+		objBD.connect();
+		objBD.query("INSERT INTO places (name) VALUES ("+ objBD.escape(newZone) +")",
+			id = function(err, rows, fields) {
+			if (err){
+	    	console.log(err);
+				res.send('3');
+			}
+		});
+		objBD.end(); 
+		zonaAdmin = newZone;
+	}
+	
+	objBD = BD.BD();
+	objBD.connect();
+	objBD.query("SELECT id_place FROM places WHERE name = "+ objBD.escape(zonaAdmin) +"",
+		function(err, rows, fields) {
+			if (err){
+	    	console.log(err);
+				res.send('3'); 
 			}
 			else 
-				res.send("2");
+				readFiles(files,rows[0].id_place,res);
+	});
+	objBD.end(); 
+	
+};
+
+
+
+// Leer archivos -------------------------------------------------------------------------------
+readFiles = function(files,idPlace,res){
+
+	if(files[0] == undefined){
+		if(path.extname(files.name).toLowerCase() === '.txt'){
+			frequency_potency = new Array();												
+			coordinate = new Array();
+			
+			lineReader.eachLine(files.path, function(line, last) {
+			  line_split = line.split("\t");	
+				
+				if(line_split.length == 2)
+					frequency_potency.push(line_split);
+					
+				else if(line_split.length == 1)
+					coordinate.push(line_split);
+			
+			  if (last){
+			  	saveArraysIntoDb(frequency_potency,coordinate,idPlace,res);
+			  	fs.unlink(files.path, function(err) {
+			    	if (err)	res.send('3'); 
+					});
+				  return false; 
+			  }
+			});	
+		}
+		else
+			res.send('2');
+	
+	} else if(files.length > 1){	
+		
+		frequency_potency = new Array();
+		coordinate = new Array();
+		
+		for(i = 0; i < files.length; i++){
+			var rd = readline.createInterface({
+		    input: fs.createReadStream(files[i].path),
+		    output: process.stdout,
+		    terminal: false
+			});
+			
+			rd.on('line', function(line) {
+				lineSplit = line.split("\t");	
+				
+				if(lineSplit.length == 2)
+					frequency_potency.push(lineSplit);
+				else if(lineSplit.length == 1)
+					coordinate.push(lineSplit);
+					
+				if(coordinate.length % 3 == 0 && coordinate.length > 0){
+					saveArraysIntoDb(frequency_potency,coordinate,idPlace,res);
+					frequency_potency = [];
+					coordinate = [];
+				}	
+			});
 
 		}
-		else		
-			res.send("1");
-		
-	} else
-	  res.send("1");	
 
-};
+
+	}
+}
+
+
+
+// Insertar vectores en DB -----------------------------------------------------------------------------------
+saveArraysIntoDb = function(frequency_potency,coordinate,idPlace,res){
+	//console.log(frequency_potency);
+		
+	objBD = BD.BD();
+	objBD.connect();
+	objBD.query("INSERT INTO coordinates (latitude, longitude, id_place, date) VALUES ("+ objBD.escape(coordinate[0]) +","+ objBD.escape(coordinate[1]) +","+ objBD.escape(idPlace) +","+ objBD.escape(coordinate[2]) +")",
+		function(err, rows, fields) {
+			objBD.end();
+	    if (err){
+	    	console.log(err);
+				res.send('3'); 
+			} else {
+				// Guardamos potencia y frecuencia en la DB --------------------------------------------------------------------------------
+				id_coordinates = rows.insertId;
+					
+				for(i = 0; i < frequency_potency.length; i++){
+					objBD = BD.BD();
+					objBD.connect();
+					objBD.query("INSERT INTO potency_frequency (frequency, potency) VALUES ("+ objBD.escape(frequency_potency[i][0]) +","+ objBD.escape(frequency_potency[i][1]) +")",
+						function(err, rows, fields) {
+							objBD.end();
+					    if (err){
+					    	console.log(err);
+								res.send('3'); 
+							} else {
+								// Guardamos id de potencia y frecuencia y de coordenadas en la tabla compartida de la DB ---------------------------------------------------------------
+								id_potency_frequency = rows.insertId;
+								
+								objBD = BD.BD();
+								objBD.connect();
+								objBD.query("INSERT INTO coordinates_vs_potency_frequency (id_potency_frequency, id_coordinate) VALUES ("+ id_potency_frequency +","+ id_coordinates +")",
+									function(err, rows, fields) {
+										objBD.end();
+								    if (err){
+								    	console.log(err);
+									    res.send('3'); 
+								    }
+								});
+							}
+					});
+				}
+			}
+	});
+
+
+}
